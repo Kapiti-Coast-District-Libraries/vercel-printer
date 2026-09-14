5d4e804b9c57bce11059cad41f470028bf872383
Kapiti-Coast-District-Libraries <paraparaumumake@gmail.com>
2026-09-14 15:43:20 +1200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
 
   return {
-    base: '/Epson-Image-Printer/',
+    base: '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
